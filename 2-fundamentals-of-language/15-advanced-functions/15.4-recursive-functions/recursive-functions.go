7f201e2f221c3212243b9d4075f54e391994ab19
William Koller <williamkoller404@gmail.com>
2022-04-04 20:22:02 -0300

    feat: add recursive functions

---
+++ 2-fundamentals-of-language/15-advanced-functions/15.4-recursive-functions/recursive-functions.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func fibonacci(position uint) uint {
+	if position <= 1 {
+		return position
+	}
+
+	return fibonacci(position - 2) + fibonacci(position - 1)
+}
+
+func main() {
+	position := uint(12)
+
+	for i := uint(0); i < position; i++ {
+		fmt.Println(fibonacci(i))
+	}
+}
