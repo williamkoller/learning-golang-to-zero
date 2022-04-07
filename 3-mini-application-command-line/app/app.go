b1cb5dd194c40402fbb5c2f51d4bdfc91329c240
William Koller <williamkoller404@gmail.com>
2022-04-06 22:50:54 -0300

    feat: add func search ips

--- 3-mini-application-command-line/app/app.go
+++ 3-mini-application-command-line/app/app.go
@@ -1,13 +1,44 @@
 package app
 
 import (
+	"fmt"
+	"log"
+	"net"
+
 	"github.com/urfave/cli"
 )
 
-// func generate will return a command line application ready to run
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "command line application"
 	app.Usage = "search IPS and server names on the internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name: "ip",
+			Usage: "IPS lookup of internet addresses",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name: "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
